331b84e3a7cbb3cf770098adb55ea5e6d8e8d0c2
Chetan M  Kittali <102372671+Neo28A@users.noreply.github.com>
2025-02-26 20:09:05 +0530

    update changes

--- frontend/src/components/MainApp.js
+++ frontend/src/components/MainApp.js
@@ -52,7 +52,7 @@ function MainApp() {
     formData.append('file', file);
 
     try {
-      const response = await fetch('http://127.0.0.1:5000/predict', {
+      const response = await fetch('https://docassist-ai-ui.onrender.com/predict', {
         method: 'POST',
         body: formData,
       });
@@ -81,7 +81,7 @@ function MainApp() {
     setDisplayedContent('');
 
     try {
-      const response = await fetch('http://127.0.0.1:5000/predict_manual', {
+      const response = await fetch('https://docassist-ai-ui.onrender.com/predict_manual', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
